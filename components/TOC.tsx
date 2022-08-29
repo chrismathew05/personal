445a75bc999673f65f1f42f504b6792a6dd4fcb7
Chris Mathew <chrismathew05@gmail.com>
2022-08-29 19:28:15 -0400

    Removed unnecessary import + created TOC file

---
+++ components/TOC.tsx
@@ -0,0 +1,41 @@
+import React, { FunctionComponent } from "react";
+import Link from "next/link";
+
+import styles from "../styles/ExtIconGroup.module.css";
+
+/**
+ * Icon group containing external links
+ */
+export const TOC: FunctionComponent = () => {
+  return (
+    <h3>
+      <a
+        href={"https://github.com/chrismathew05/"}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.icon}
+      >
+        <FaGithub />
+      </a>
+      <a
+        href={"https://ca.linkedin.com/in/chris-mathew-0bbb26128"}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.icon}
+      >
+        <FaLinkedin />
+      </a>
+      <a
+        href={"https://github.com/chrismathew05/"}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.icon}
+      >
+        <FaFileAlt />
+      </a>
+      <a href={"mailto:chrismathew05@gmail.com"} className={styles.icon}>
+        <FaEnvelope />
+      </a>
+    </h3>
+  );
+};
