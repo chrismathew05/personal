610a33e42573d642eea1d4cbaf29b5eda0317965
Chris Mathew <chrismathew05@gmail.com>
2022-08-30 11:51:50 -0400

    Added layout for Skills section

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 
 import { TOC } from "../components/TOC";
 import { About } from "../components/About";
+import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
 import { Education } from "../components/Education";
 import { Experience } from "../components/Experience";
