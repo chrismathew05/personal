783bf269dfeb3fe0dd3bde4a0ed08ca98ce8f8af
Chris Mathew <chrismathew05@gmail.com>
2022-08-31 14:53:37 -0400

    Added flow diagram from PC project

--- components/elements/Projects.tsx
+++ components/elements/Projects.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from "react";
+import Image from "next/image";
 
 import { ExtLinks } from "../utility/ExtLinks";
 import { Pills } from "../utility/Pills";
@@ -14,7 +15,7 @@ export const Projects: FunctionComponent = () => (
         "https://od.meteorapp.com/",
         "https://od.meteorapp.com/developer",
         "#",
-        "#",
+        "https://www.youtube.com/watch?v=4EGWN8SdcyE&list=PLJD5A0Xn9q_mJQtCAhv0YSUCJjO0iDq_d&ab_channel=0verdriveConnect",
       ]}
       labels={["Website", "API", "Github", "Demo"]}
       keyId="pdflow"
@@ -31,7 +32,9 @@ export const Projects: FunctionComponent = () => (
       to partition large extraction jobs. Text extraction is performed via the
       Tesseract OCR Engine. Image pre-processing (grayscale, otsu threshold) is
       used to improve the accuracy of the extraction. Developers can also
-      utilize the Pdflow API to integrate flows into their own apps.
+      utilize the Pdflow API to integrate flows into their own apps. To
+      accomodate Tesseract, I built a Docker image which gets uploaded to AWS
+      ECR and then deployed to Lambda.
     </p>
     <p>
       Pdflow has over 50 users consisting of students, educators, business
@@ -66,9 +69,9 @@ export const Projects: FunctionComponent = () => (
     <p>
       &quot;Gourd&quot; was built by me as an internal ERP for our start-up,
       Intelline, before transitioning to Quickbooks. While most sane people
-      would have started with an existing ERP to run their business, I was
-      determined to improve my web dev skills while building an useful, tailored
-      internal tool for our company.
+      would have used an existing ERP to run their business, I was determined to
+      improve my web dev skills while building an useful, tailored internal tool
+      for our company.
     </p>
     <p>
       Gourd includes modules for: recording/tracking tasks, expenditure
@@ -82,14 +85,61 @@ export const Projects: FunctionComponent = () => (
         "Meteor",
         "React",
         "MongoDB",
-        "Google Drive",
+        "GDrive",
         "Tableau",
         "Heroku",
       ]}
       keyId="pdflow-skills"
     />
 
-    <h3>Stock Tracker</h3>
+    <h3 style={{ marginBottom: "0px" }}>Stock Project</h3>
+    <ExtLinks
+      extLinks={[
+        "https://gourd.herokuapp.com/",
+        "https://drive.google.com/file/d/1jU5aN5QjHRmMWUdcc205B8iX-svi8xGZ/view?usp=sharing",
+      ]}
+      labels={["Github", "Docs"]}
+      keyId="pdflow"
+    />
+    <p>
+      I started this project with my friend{" "}
+      <a
+        href="https://ca.linkedin.com/in/tobiaspushparajah"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <u>Tobias</u>
+      </a>{" "}
+      after listening to a lecture at UW about stock price reactions to earnings
+      releases. This project scrapes various sources (Yahoo Finance, Tipranks,
+      Questrade, etc.) and tracks daily/intraday metrics on stocks from the
+      following markets: CSE, TSX, TSXV, NASDAQ, NYSE. A scoring algorithm is
+      continuously run, sending us notifications of what to buy/sell.
+    </p>
+    <p>
+      The scoring algorithm is quite naive at the moment. I am considering using
+      a supervised learning algorithm (likely an ensemble) to help build a model
+      between the metrics I track and stock performance.
+    </p>
+    <Image src={"/pc-flow.png"} width="1870px" height="1320px" alt="test" />
+    <Pills
+      labels={[
+        "Javascript",
+        "NextJS",
+        "React",
+        "CockroachDB",
+        "SQL",
+        "AWS",
+        "Python",
+        "Numpy",
+        "Pandas",
+        "Selenium",
+        "Requests",
+        "Bash",
+      ]}
+      keyId="pdflow-skills"
+    />
+
     <h3>ML Notebooks</h3>
     <h3>Other</h3>
   </section>
